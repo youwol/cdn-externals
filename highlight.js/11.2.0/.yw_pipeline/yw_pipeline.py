fa4dbe0dabcdea8687ee6659eaab66c4c8827f8f
youwol-guillaume <greinisch@youwol.com>
2022-09-09 18:22:34 +0200

    :bug: highlight.js => copy styles folder when doing 'init'

--- highlight.js/11.2.0/.yw_pipeline/yw_pipeline.py
+++ highlight.js/11.2.0/.yw_pipeline/yw_pipeline.py
@@ -1,9 +1,15 @@
 from youwol.environment.forward_declaration import YouwolEnvironment
 from youwol.environment.models import IPipelineFactory
-from youwol.environment.models_project import Pipeline
+from youwol.environment.models_project import Pipeline, Flow
+from youwol.pipelines.pipeline_typescript_weback_npm import BuildStep
 from youwol.pipelines.pipeline_typescript_weback_npm.external import PipelineConfig, pipeline
 
 from youwol_utils.context import Context
+
+
+class PatchInitStep(BuildStep):
+    id: str = "init_patched"
+    run: str = f"yarn && cp -r ./node_modules/highlight.js/styles ./styles"
 
 
 class PipelineFactory(IPipelineFactory):
@@ -12,5 +18,15 @@ class PipelineFactory(IPipelineFactory):
         super().__init__(**kwargs)
 
     async def get(self, _env: YouwolEnvironment, ctx: Context) -> Pipeline:
+        base_pipeline = await pipeline(PipelineConfig(), ctx)
+        base_pipeline.steps.append(PatchInitStep())
+        base_pipeline.flows = [
+            Flow(
+                name="prod",
+                dag=[
+                    "init_patched > build > publish-local > publish-remote "
+                ]
+            )
+        ]
+        return base_pipeline
 
-        return await pipeline(PipelineConfig(), ctx)
