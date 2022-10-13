883e6c3ebbd71e2d440c384c26969b45400d6765
youwol-guillaume <greinisch@youwol.com>
2022-10-13 07:38:41 +0200

    :wrench: update yw_pipeline.py

--- fontawesome/5.12.1/.yw_pipeline/yw_pipeline.py
+++ fontawesome/5.12.1/.yw_pipeline/yw_pipeline.py
@@ -2,7 +2,7 @@ from typing import List
 from youwol.environment.forward_declaration import YouwolEnvironment
 from youwol.environment.models import IPipelineFactory
 from youwol.environment.models_project import Artifact, Flow, Pipeline, PipelineStep, FileListing, JsBundle
-from youwol.pipelines.pipeline_typescript_weback_npm import PublishCdnRemoteStep, PublishCdnLocalStep
+from youwol.pipelines.pipeline_typescript_weback_npm import PublishCdnLocalStep, create_sub_pipelines_publish_cdn
 from youwol_utils.context import Context
 from youwol_utils.utils_paths import parse_json
 
@@ -31,7 +31,8 @@ class PipelineFactory(IPipelineFactory):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    async def get(self, _env: YouwolEnvironment, _ctx: Context):
+    async def get(self, _env: YouwolEnvironment, context: Context):
+        steps, dags = await create_sub_pipelines_publish_cdn(start_step="cdn-local", context=context)
 
         return Pipeline(
             target=JsBundle(),
@@ -41,14 +42,13 @@ class PipelineFactory(IPipelineFactory):
             steps=[
                 BuildStep(),
                 PublishCdnLocalStep(packagedArtifacts=['dist']),
-                PublishCdnRemoteStep()
-            ],
+            ] + steps,
             flows=[
                 Flow(
                     name="prod",
                     dag=[
                         "build > cdn-local > publish-remote "
-                    ]
+                    ] + dags
                 )
             ]
         )
