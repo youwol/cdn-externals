6971888ace77ace43226e976b725b700caefb19d
youwol-guillaume <greinisch@youwol.com>
2022-11-23 11:20:26 +0100

    ✨ add marked#4.2.3

---
+++ marked/4.2.3/src/index.ts
@@ -0,0 +1,14 @@
+export * from "marked";
+/*
+
+
+
+return async ({debug}) => {
+    const CDN = window['@youwol/cdn-client']
+    const FluxView = window['@youwol/flux-view']
+    await CDN.install({modules:["marked#^4.2.3"]})
+
+    return true
+}
+
+ */
