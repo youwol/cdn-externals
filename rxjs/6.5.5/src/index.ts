18c424efb3677b5748d5e2d19470ad9cf2fecb6d
youwol-guillaume <greinisch@youwol.com>
2023-02-21 15:25:23 +0100

    ✨ rxjs => export 'fetch'

--- rxjs/6.5.5/src/index.ts
+++ rxjs/6.5.5/src/index.ts
@@ -1,6 +1,8 @@
 export * from "rxjs";
 export * as operators from "rxjs/operators";
+export * as fetch from "rxjs/fetch";
 import * as rxjs from "rxjs";
 import * as operators from "rxjs/operators";
+import * as fetch from "rxjs/fetch";
 
-window["rxjs_APIv6"] = { ...rxjs, operators };
+window["rxjs_APIv6"] = { ...rxjs, operators, fetch };
