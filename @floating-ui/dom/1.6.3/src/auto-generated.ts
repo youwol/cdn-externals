7fa5062e55a906b5b22f3021f0df5d935b69f890
youwol-guillaume <greinisch@youwol.com>
2024-03-12 04:53:27 +0100

    ✨ `@floating-ui/dom#1.6.3`

---
+++ @floating-ui/dom/1.6.3/src/auto-generated.ts
@@ -0,0 +1,25 @@
+
+const runTimeDependencies = {
+    "externals": {},
+    "includedInBundle": {
+        "@floating-ui/dom": "1.6.3"
+    }
+}
+const externals = {}
+const exportedSymbols = {}
+
+const entries = {
+    '@floating-ui/dom': './index.ts',
+}
+export const setup = {
+    name:'@floating-ui/dom',
+    assetId:'QGZsb2F0aW5nLXVpL2RvbQ==',
+    version:'1.6.3',
+    shortDescription:"Floating UI for the web",
+    apiVersion:'1',
+    runTimeDependencies,
+    externals,
+    exportedSymbols,
+    entries,
+}
+
---
+++ @floating-ui/dom/1.6.3/src/index.ts
@@ -0,0 +1,6 @@
+import DefaultImport from "@floating-ui/dom"
+export {setup as webpmSetup} from './auto-generated'
+// It may be the case that one of the two following line is not needed
+export * from "@floating-ui/dom"
+export default DefaultImport
+
