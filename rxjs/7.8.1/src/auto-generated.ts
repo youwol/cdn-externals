4cef8ffb827b063d06ed813dcb339dcb18c71ccc
youwol-guillaume <greinisch@youwol.com>
2024-04-10 08:47:19 +0200

    ✨ `rxjs#7.8.1`

---
+++ rxjs/7.8.1/src/auto-generated.ts
@@ -0,0 +1,25 @@
+
+const runTimeDependencies = {
+    "externals": {},
+    "includedInBundle": {
+        "rxjs": "7.8.1"
+    }
+}
+const externals = {}
+const exportedSymbols = {}
+
+const entries = {
+    'rxjs': './index.ts',
+}
+export const setup = {
+    name:'rxjs',
+    assetId:'cnhqcw==',
+    version:'7.8.1',
+    shortDescription:"Reactive Extensions for modern JavaScript",
+    apiVersion:'7',
+    runTimeDependencies,
+    externals,
+    exportedSymbols,
+    entries,
+}
+
---
+++ rxjs/7.8.1/src/index.ts
@@ -0,0 +1,11 @@
+export { setup as webpmSetup } from './auto-generated'
+export * from 'rxjs'
+export { default } from 'rxjs'
+
+export * as ajax from 'rxjs/ajax'
+export * as fetch from 'rxjs/fetch'
+export * as operators from 'rxjs/operators'
+export * as testing from 'rxjs/testing'
+export * as webSocket from 'rxjs/webSocket'
+
+// export * as a from 'rxjs/internals'
