55f4af8ef47454bb84fc6582c1e088d9335c341d
youwol-guillaume <greinisch@youwol.com>
2024-05-01 08:47:52 +0200

    ✨ `esprima#4.0.1`

---
+++ esprima/4.0.1/src/auto-generated.ts
@@ -0,0 +1,25 @@
+
+const runTimeDependencies = {
+    "externals": {},
+    "includedInBundle": {
+        "esprima": "4.0.1"
+    }
+}
+const externals = {}
+const exportedSymbols = {}
+
+const entries = {
+    'esprima': './index.ts',
+}
+export const setup = {
+    name:'esprima',
+    assetId:'ZXNwcmltYQ==',
+    version:'4.0.1',
+    shortDescription:"ECMAScript parsing infrastructure for multipurpose analysis",
+    apiVersion:'4',
+    runTimeDependencies,
+    externals,
+    exportedSymbols,
+    entries,
+}
+
---
+++ esprima/4.0.1/src/index.ts
@@ -0,0 +1,6 @@
+import DefaultImport from "esprima"
+export {setup as webpmSetup} from './auto-generated'
+// It may be the case that one of the two following line is not needed
+export * from "esprima"
+export default DefaultImport
+
