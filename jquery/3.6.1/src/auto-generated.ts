51600cf231128ea7c98f9e7db8817ae9fe5d1d0c
youwol-guillaume <greinisch@youwol.com>
2024-07-05 09:00:40 +0200

    ♻️ `jquery#3.6.1`

---
+++ jquery/3.6.1/src/auto-generated.ts
@@ -0,0 +1,25 @@
+
+const runTimeDependencies = {
+    "externals": {},
+    "includedInBundle": {
+        "jquery": "3.6.1"
+    }
+}
+const externals = {}
+const exportedSymbols = {}
+
+const entries = {
+    'jquery': './index.ts',
+}
+export const setup = {
+    name:'jquery',
+    assetId:'anF1ZXJ5',
+    version:'3.6.1',
+    shortDescription:"JavaScript library for DOM operations",
+    apiVersion:'3',
+    runTimeDependencies,
+    externals,
+    exportedSymbols,
+    entries,
+}
+
--- jquery/3.6.1/src/index.ts
+++ jquery/3.6.1/src/index.ts
@@ -1,9 +1,4 @@
-//
-// import $ from 'jquery'
-// window["$#3"] = $
-
-import DefaultImport from "jquery";
-
-export * from "jquery";
-export default DefaultImport;
-window["$_APIv3"] = DefaultImport;
+import DefaultImport from 'jquery'
+export { setup as webpmSetup } from './auto-generated'
+export * from 'jquery'
+export default DefaultImport
